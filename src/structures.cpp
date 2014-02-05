ee8c2b82b0956969ab37d8d6a7a240ba3bd33f14
Tom Cornebize <tom@tom-netbook.(none)>
2014-02-05 12:12:14 +0100

    Maj structure de données.

--- src/structures.cpp
+++ src/structures.cpp
@@ -7,19 +7,68 @@
 using namespace satsolver;
 
 Clause::Clause(int nb_variables, bool *literals) {
-    m_nb_variables = nb_variables;
-    m_literals = literals;
+    this->m_literals = literals;
+    this->m_nb_variables = nb_variables ;
+    this->m_size = 0 ;
+    for(int i = 0 ; i < nb_variables*2 ; i++) {
+    	if(literals[i])
+    		this->m_size ++ ;
+    }
 }
 
+/*
 Clause::Clause(int nb_variables, std::vector<int> literals) {
     m_literals = (bool*) malloc(nb_variables*2 + 1);
     memset(m_literals, false, nb_variables*2 + 1);
     for(std::vector<int>::iterator it = literals.begin(); it != literals.end(); ++it) {
         m_literals[*it] = true;
     }
+}*/
+
+Clause::Clause(int nb_variables, std::vector<int> literals) {
+		int sub ;
+    this->m_literals = (bool*) malloc(nb_variables*2);
+    memset(this->m_literals, false, nb_variables*2);
+    for(std::vector<int>::iterator it = literals.begin(); it != literals.end(); ++it) {
+    		assert(*it != 0) ;
+    		if(*it > 0) 
+    			sub = 1 ;
+    		else
+    			sub = 0 ;
+        this->m_literals[nb_variables + *it - sub] = true;
+    }
 }
 
 bool Clause::contains_literal(int literal) {
-    assert(literal+m_nb_variables<2*m_nb_variables);
-    return m_literals[literal+m_nb_variables];
+		int sub ;
+    assert(literal != 0 && abs(literal) <= m_nb_variables);
+    if(literal > 0)
+    	sub = 1 ;
+    else
+    	sub = 0 ;
+    return this->m_literals[this->m_nb_variables + literal - sub];
+}
+
+void Clause::add(int literal) {
+		int sub ;
+		if(!this->contains_literal(literal)) {
+			this->m_size ++ ;
+			if(literal > 0)
+				sub = 1 ;
+			else
+				sub = 0 ;
+			this->m_literals[this->m_nb_variables + literal - sub] = true ;
+		}
+}	
+
+void Clause::remove(int literal) {
+		int sub ;
+		if(this->contains_literal(literal)) {
+			this->m_size -- ;
+			if(literal > 0)
+				sub = 1 ;
+			else
+				sub = 0 ;
+			this->m_literals[this->m_nb_variables + literal - sub] = false ;
+		}
 }
--- src/structures.h
+++ src/structures.h
@@ -4,14 +4,22 @@ namespace satsolver {
 
 class Clause {
     private:
-        int m_nb_variables;
-        bool *m_literals;
+        int m_size; 				// nombre de literaux dans la clause
+        bool *m_literals;		// clause 
+        int m_nb_variables;	// nombre de variables dans le problème				
 
     public:
         Clause(int nb_variables, bool *literals);
         Clause(int nb_variables, std::vector<int> literals);
-
+				
+				// Test si le litéral donné appartient à la clause
         bool contains_literal(int literal);
+        
+        // Aoute le litéral donné à la clause
+        void add(int literal) ;
+        
+        // Supprime le litéral donné de la clause
+        void remove(int literal) ;
 };
 
 }
