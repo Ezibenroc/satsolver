92e705a8bbd706ea738f94f19ac8f26a36d56504
Valentin Lorentz <progval@gmail.com>
2014-03-12 21:39:53 +0100

    Merge resol and resol-wl.

--- src/entry_points/resol.cpp
+++ src/entry_points/resol.cpp
@@ -1,5 +1,79 @@
-#include "entry_points/resol_common.h"
+#include <set>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+
+#include "structures/affectation.h"
+#include "structures/formula.h"
+#include "parsers/sat.h"
+#include "solvers/dpll.h"
+#include "config.h"
+
+bool VERBOSE = false;
+bool WITH_WL = false;
+
+void bad_command_options(char *executable) {
+    std::cout << "Syntax: " << executable << " [-verbose] [-WL] [<filename>]\n\n";
+    std::cout << "If filename is not given, stdin is used instead." << std::endl;
+}
 
 int main(int argc, char *argv[]) {
-    return satsolver::resol_main(argc, argv, false);
+    srand((unsigned int) time(NULL)) ;
+    satsolver::SatParser *parser;
+    satsolver::Formula *formula;
+    satsolver::Affectation *solution;
+    std::set<int> *solution_set;
+    std::set<int>::iterator solution_set_iterator;
+    int i;
+    bool using_stdin = true;
+    std::istream *input = &std::cin;
+    for (i=1; i<argc && argv[i][0]=='-'; i++) {
+        if (!strcmp(argv[i], "-verbose"))
+            VERBOSE = true;
+        else if (!strcmp(argv[i], "-WL"))
+            WITH_WL = true;
+        else {
+            bad_command_options(argv[0]);
+            return 1;
+        }
+    }
+    if (i == argc) {
+        // No other option
+    }
+    else if (i == argc-1) {
+        // One option left; hopefully the file name
+        input = new std::ifstream(argv[i]);
+        using_stdin = false;
+    }
+    else {
+        // Too many unknown options
+        bad_command_options(argv[0]);
+        return 1;
+    }
+    parser = new satsolver::SatParser(*input);
+    parser->parse();
+    formula = parser->get_formula();
+    try {
+        solution = satsolver::solve(formula);
+    }
+    catch (satsolver::Conflict e) {
+        std::cout << "s UNSATISFIABLE" << std::endl;
+        return 1;
+    }
+
+    std::cout << "s SATISFIABLE" << std::endl;
+    solution_set = solution->to_set();
+    for (solution_set_iterator=solution_set->begin(); solution_set_iterator!=solution_set->end(); solution_set_iterator++) {
+        std::cout << "v " << *solution_set_iterator << std::endl;
+    }
+
+    if (!using_stdin)
+        delete input;
+    delete parser;
+    delete solution_set ;
+    delete formula;
+    return 0;
 }
