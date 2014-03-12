38ea9de23903625f452b306e767c50a75cf99515
Valentin Lorentz <progval@gmail.com>
2014-03-12 21:31:09 +0100

    Improve extented formula reduction.

--- src/structures/extended_formula.cpp
+++ src/structures/extended_formula.cpp
@@ -13,6 +13,13 @@ using namespace satsolver;
 
 int EF::last_id = 0;
 
+EF::EF(const EF *that) : type(that->type), f1(that->f1), f2(that->f2), literal(that->literal), id(++this->last_id) {
+}
+
+EF::EF(EF::Type type) : type (type), id(++this->last_id) {
+    assert(type == EF::TRUE || type == EF::FALSE);
+}
+
 EF::EF(EF::Type type, std::string literal) : type (type), literal(literal), id(++this->last_id) {
     assert(type == EF::LITERAL);
 }
@@ -22,19 +29,24 @@ EF::EF(EF::Type type, SPEF f) : type (type), f1(f), id(++this->last_id) {
 }
 
 EF::EF(EF::Type type, SPEF f1, SPEF f2) : type (type), f1(f1), f2(f2), id(++this->last_id) {
+    assert(type != EF::TRUE);
+    assert(type != EF::FALSE);
     assert(type != EF::LITERAL);
     assert(type != EF::NOT);
 }
 
-std::string EF::make_literal() {
-    return std::string("@") + std::to_string(this->id);
+std::string EF::make_literal() const {
+    if (this->type == EF::LITERAL)
+        return this->literal;
+    else
+        return std::string("@") + std::to_string(this->id);
 }
 
-std::vector<std::vector<std::string>*>* EF::reduce() {
+std::vector<std::vector<std::string>*>* EF::reduce() const {
     std::vector<std::vector<std::string>*> *result = new std::vector<std::vector<std::string>*>(), *f1_reduction, *f2_reduction;
     std::vector<std::string> *clause;
 
-    if (type == EF::LITERAL) {
+    if (type == EF::LITERAL || type == EF::TRUE || type == EF::FALSE) {
     }
     else if (type == EF::NOT) {
         f1_reduction = this->f1->reduce();
@@ -134,12 +146,20 @@ std::vector<std::vector<std::string>*>* EF::reduce() {
             clause = new std::vector<std::string>{this->literal, "", ""};
             result->push_back(clause);
             break;
+        case TRUE:
+            break;
+        case FALSE:
+            clause = new std::vector<std::string>{this->make_literal(), "", ""};
+            result->push_back(clause);
+            clause = new std::vector<std::string>{"-" + this->make_literal(), "", ""};
+            result->push_back(clause);
+            break;
     }
     return result;
 }
 
 
-std::shared_ptr<Formula> EF::reduce_to_formula(std::shared_ptr<std::map<std::string, int>> *name_to_variable_ptr) {
+std::shared_ptr<Formula> EF::reduce_to_formula(std::shared_ptr<std::map<std::string, int>> *name_to_variable_ptr) const {
     std::vector<std::vector<std::string>*> *raw_clauses = this->reduce();
     std::shared_ptr<std::map<std::string, int>> name_to_variable = std::make_shared<std::map<std::string, int>>();
     int nb_variables = 0;
@@ -170,12 +190,141 @@ std::shared_ptr<Formula> EF::reduce_to_formula(std::shared_ptr<std::map<std::str
                 int_literals->push_back(name_to_variable->at(string_literal));
             }
         }
+        assert(int_literals->size());
         clauses.push_back(std::shared_ptr<satsolver::Clause>(new satsolver::Clause(nb_variables, *int_literals)));
     }
+    assert(clauses.size());
     
     if (name_to_variable_ptr)
         *name_to_variable_ptr = name_to_variable;
     return std::shared_ptr<Formula>(new Formula(clauses, nb_variables));
 
 
+}
+
+std::string EF::to_string() const {
+    switch (this->type) {
+        case EF::OR:
+            if (this->f1->type == EF::TRUE || this->f2->type == EF::TRUE)
+                return "⊤";
+            else if (this->f1->type == EF::FALSE)
+                return this->f2->to_string();
+            else if (this->f2->type == EF::FALSE)
+                return this->f1->to_string();
+            break;
+        case EF::AND:
+            if (this->f1->type == EF::FALSE || this->f2->type == EF::FALSE)
+                return "⊥";
+            else if (this->f1->type == EF::TRUE)
+                return this->f2->to_string();
+            else if (this->f2->type == EF::TRUE)
+                return this->f1->to_string();
+            break;
+        default:
+            break;
+    }
+    switch (this->type) {
+        case EF::XOR:
+            return "(" + this->f1->to_string() + "⊕" + this->f2->to_string() + ")";
+        case EF::OR:
+            return "(" + this->f1->to_string() + "+" + this->f2->to_string() + ")";
+        case EF::AND:
+            return "(" + this->f1->to_string() + "•" + this->f2->to_string() + ")";
+        case EF::IMPLIES:
+            return "(" + this->f1->to_string() + "→" + this->f2->to_string() + ")";
+        case EF::NOT:
+            return "(¬" + this->f1->to_string() + ")";
+        case EF::LITERAL:
+            return "“" + this->literal + "”";
+        case EF::TRUE:
+            return "⊤";
+        case EF::FALSE:
+            return "⊥";
+    }
+    assert(false);
+}
+
+bool EF::operator==(const ExtendedFormula &that) const {
+    if (this->type != that.type) {
+        return false;
+    }
+    switch (this->type) {
+        case EF::OR:
+        case EF::XOR:
+        case EF::AND:
+            return ((*this->f1 == *that.f1 && *this->f2 == *that.f2) || (*this->f1 == *that.f2 && *this->f2 == *that.f1));
+        case EF::IMPLIES:
+            return (*this->f1 == *that.f1 && *this->f2 == *that.f2);
+        case EF::NOT:
+            return *this->f1 == *that.f1;
+        case EF::LITERAL:
+            return this->literal == that.literal;
+        case TRUE:
+        case FALSE:
+            return true;
+    }
+    assert(false);
+}
+
+bool EF::operator!=(const ExtendedFormula &that) const {
+    return !(*this == that);
+}
+
+SPEF EF::simplify() const {
+    SPEF f1, f2;
+    switch(this->type) {
+        case EF::OR:
+        case EF::AND:
+        case EF::XOR:
+        case EF::IMPLIES:
+            f1 = this->f1->simplify();
+            f2 = this->f2->simplify();
+            break;
+        case EF::NOT:
+            f1 = this->f1->simplify();
+            break;
+        default:
+            break;
+    }
+    switch (this->type) {
+        case EF::OR:
+            if (*f1 == EF(EF::NOT, f2))
+                return SPEF(new EF(EF::TRUE));
+            else if (*f2 == EF(EF::NOT, f1))
+                return SPEF(new EF(EF::TRUE));
+            else if (f1->type == EF::FALSE)
+                return SPEF(new EF(*f2));
+            else if (f2->type == EF::FALSE)
+                return SPEF(new EF(*f1));
+            break;
+        case EF::AND:
+            if (*f1 == *f2)
+                return SPEF(new EF(*f1));
+            else if (f1->type == TRUE)
+                return SPEF(new EF(*f2));
+            else if (f2->type == TRUE)
+                return SPEF(new EF(*f1));
+            if (*f1 == EF(EF::NOT, f2))
+                return SPEF(new EF(EF::FALSE));
+            else if (*f2 == EF(EF::NOT, f1))
+                return SPEF(new EF(EF::FALSE));
+            break;
+        default:
+            break;
+    }
+    switch(this->type) {
+        case EF::OR:
+        case EF::AND:
+        case EF::XOR:
+        case EF::IMPLIES:
+            return SPEF(new EF(this->type, f1, f2));
+        case EF::NOT:
+            if (f1->type == EF::NOT)
+                return SPEF(new EF(*f1->f1));
+            else
+                return SPEF(new EF(this->type, f1));
+        default:
+            return SPEF(new EF(this));
+    }
+    assert(false);
 }
