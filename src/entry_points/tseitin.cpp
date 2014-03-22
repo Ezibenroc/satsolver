d98304adca05c868eced0970eefafd98ca9f1a12
Valentin Lorentz <progval@gmail.com>
2014-03-22 19:34:53 +0100

    Fix handling of unsatisfiable formulas in tseitin.

--- src/entry_points/tseitin.cpp
+++ src/entry_points/tseitin.cpp
@@ -21,13 +21,19 @@ bool DISPLAY_SAT;
 satsolver::Heuristic HEURISTIC = satsolver::DUMB ;
 
 void parser_result(SPEF ext_formula) {
+    satsolver::Affectation *sat_solution;
     std::shared_ptr<std::map<std::string, int>> name_to_variable;
     std::shared_ptr<std::unordered_set<std::string>> literals = ext_formula->get_literals();
 
     ext_formula = ext_formula->simplify();
     if (VERBOSE)
         std::cout << "Reduction of formula to: " << ext_formula->to_string() << std::endl;
-    std::shared_ptr<satsolver::Formula> formula = ext_formula->reduce_to_formula(&name_to_variable);
+    std::shared_ptr<satsolver::Formula> formula = NULL;
+    try {
+        formula = ext_formula->reduce_to_formula(&name_to_variable);
+    }
+    catch (satsolver::Conflict) {
+    }
     if (!formula) { // The formula is always false
         if (DISPLAY_SAT)
             std::cout << "c The formula is so obviously wrong it is not even needed to convert it to conjonctive form." << std::endl;
@@ -47,11 +53,17 @@ void parser_result(SPEF ext_formula) {
         }
         std::cout << "c End of formula\n" << std::endl ;
     }
-    satsolver::Affectation sat_solution = satsolver::solve(&*formula); // May raise a satsolver::Conflict
+    try {
+        sat_solution = satsolver::solve(&*formula);
+    }
+    catch (satsolver::Conflict) {
+        std::cout << "s UNSATISFIABLE" << std::endl;
+        return;
+    }
     if (VERBOSE)
-        std::cout << "Solution to SAT problem: " << sat_solution.to_string() << std::endl;
+        std::cout << "Solution to SAT problem: " << sat_solution->to_string() << std::endl;
     for (auto literal : *literals) {
-        std::cout << literal << " = " << (sat_solution.is_true(name_to_variable->at(literal)) ? "true" : "false") << std::endl;
+        std::cout << literal << " = " << (sat_solution->is_true(name_to_variable->at(literal)) ? "true" : "false") << std::endl;
     }
 }
 
