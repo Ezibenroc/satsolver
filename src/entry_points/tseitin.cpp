f60794b2110d38f2ea0b28f7771ae1c33c763a11
Valentin Lorentz <progval@gmail.com>
2014-03-22 20:38:57 +0100

    Remove ambiguity in the ExtendedFormula parser.

--- src/entry_points/tseitin.cpp
+++ src/entry_points/tseitin.cpp
@@ -18,6 +18,7 @@ extern int yyparse();
 bool VERBOSE = false;
 bool WITH_WL = false;
 bool DISPLAY_SAT;
+bool DISPLAY_FORMULA;
 satsolver::Heuristic HEURISTIC = satsolver::DUMB ;
 
 void parser_result(SPEF ext_formula) {
@@ -25,6 +26,8 @@ void parser_result(SPEF ext_formula) {
     std::shared_ptr<std::map<std::string, int>> name_to_variable;
     std::shared_ptr<std::unordered_set<std::string>> literals = ext_formula->get_literals();
 
+    if (VERBOSE || DISPLAY_FORMULA)
+        std::cout << "Interpreted formula as: " << ext_formula->to_string() << std::endl;
     ext_formula = ext_formula->simplify();
     if (VERBOSE)
         std::cout << "Reduction of formula to: " << ext_formula->to_string() << std::endl;
@@ -68,7 +71,7 @@ void parser_result(SPEF ext_formula) {
 }
 
 int main (int argc, char *argv[]) {
-    CommandLineParser cli_parser(argc, argv, std::unordered_set<std::string>({"-print-sat"}), "[-print-sat] [<filename>]");
+    CommandLineParser cli_parser(argc, argv, std::unordered_set<std::string>({"-print-interpretation", "-print-sat"}), "[-print-sat] [<filename>]");
     if (cli_parser.get_nb_parsed_args() == -1)
         return 1;
     int nb_remaining_args = argc - cli_parser.get_nb_parsed_args();
@@ -85,5 +88,6 @@ int main (int argc, char *argv[]) {
         return 1;
     }
     DISPLAY_SAT = cli_parser.get_arg("-print-sat");
+    DISPLAY_FORMULA = cli_parser.get_arg("-print-interpretation");
     return yyparse();
 }
--- src/parsers/extended_formula.y
+++ src/parsers/extended_formula.y
@@ -27,7 +27,7 @@ void parser_result(SPEF formula);
 
 %start whole_formula
 
-%type <type_formula> formula
+%type <type_formula> formula formula_not formula_and formula_or formula_implies
 %type <type_string> VAR
 
 %%
@@ -35,13 +35,24 @@ whole_formula:
       formula { parser_result(SPEF((EF*) $1)); }
     ;
 
-formula:
+formula_not:
       LEFT_PAREN formula RIGHT_PAREN { $$ = $2; }
     | VAR { $$ = new EF(EF::LITERAL, std::string($1)); }
-    | NOT formula { $$ = new EF(EF::NOT, SPEF((EF*) $2)); }
-    | formula OR formula { $$ = new EF(EF::OR, SPEF((EF*) $1), SPEF((EF*) $3)); }
-    | formula AND formula { $$ = new EF(EF::AND, SPEF((EF*) $1), SPEF((EF*) $3)); }
-    | formula IMPLIES formula { $$ = new EF(EF::IMPLIES, SPEF((EF*) $1), SPEF((EF*) $3)); }
+    | NOT formula_not { $$ = new EF(EF::NOT, SPEF((EF*) $2)); }
+formula_and:
+      formula_not { $$ = $1; }
+    | formula_not AND formula_and { $$ = new EF(EF::AND, SPEF((EF*) $1), SPEF((EF*) $3)); }
+    ;
+formula_or:
+      formula_and { $$ = $1; }
+    | formula_and OR formula_or { $$ = new EF(EF::OR, SPEF((EF*) $1), SPEF((EF*) $3)); }
+    ;
+formula_implies:
+      formula_or { $$ = $1; }
+    | formula_or IMPLIES formula_implies { $$ = new EF(EF::IMPLIES, SPEF((EF*) $1), SPEF((EF*) $3)); }
+    ;
+formula:
+      formula_implies { $$ = $1; }
     ;
 
 %%
