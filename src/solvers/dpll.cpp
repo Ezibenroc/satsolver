71ead4e2e568041d17e433e148bb4e4522bddfba
Valentin Lorentz <progval@gmail.com>
2014-04-06 20:10:49 +0200

    Change the conflict graph algorithm to work with non-absolute value (ie. the actually deduced one).

--- src/solvers/dpll.cpp
+++ src/solvers/dpll.cpp
@@ -15,41 +15,56 @@
 
 using namespace satsolver;
 
-void write_conflict_graph(std::map<unsigned int, std::set<unsigned int>> deductions, unsigned int literal) {
-    FILE *graph_file = fopen(CONFLICT_GRAPH_FILE_NAME, "w");
-    std::set<unsigned int> handled, to_be_handled({literal});
-    fprintf(graph_file, "digraph G {\n");
+#define AS_AFF(a, l) (aff.is_true(l) ? l : -l)
+
+void handle_literal(FILE *graph_file, std::map<int, std::set<int>> deductions, std::set<int> *handled, std::set<int> &to_be_handled, Affectation &aff, int literal) {
+    if (deductions.find(literal) == deductions.end())
+        return;
+    for (auto it : deductions.at(literal)) {
+        if (it == literal || it == -literal)
+            continue;
+        to_be_handled.insert(it);
+        if (handled->find(it) != handled->end() || handled->find(-it) != handled->end())
+            continue;
+        if (graph_file)
+            fprintf(graph_file, "\t\"%d\" -> \"%d\";\n", AS_AFF(aff, it), AS_AFF(aff, literal));
+    }
+}
+
+std::set<int>* make_conflict_graph(std::map<int, std::set<int>> deductions, Affectation &aff, int literal, bool write) {
+    FILE *graph_file = NULL;
+    std::set<int> *handled = new std::set<int>(), to_be_handled({literal});
+    if (write) {
+        graph_file = fopen(CONFLICT_GRAPH_FILE_NAME, "w");
+        fprintf(graph_file, "digraph G {\n");
+    }
     while (to_be_handled.size()) {
         literal = *to_be_handled.begin();
         to_be_handled.erase(to_be_handled.begin());
-        if (handled.find(literal) != handled.end())
+        if (handled->find(literal) != handled->end() || handled->find(-literal) != handled->end())
             continue;
-        handled.insert(literal);
-        if (deductions.find(literal) != deductions.end()) {
-            for (auto it : deductions.at(literal)) {
-                if (it == literal)
-                    continue;
-                to_be_handled.insert(it);
-                if (handled.find(it) != handled.end())
-                    continue;
-                fprintf(graph_file, "\t\"%u\" -> \"%u\";\n", it, literal);
-            }
-        }
+        handled->insert(literal);
+        assert(!aff.is_unknown(abs(literal)));
+        handle_literal(graph_file, deductions, handled, to_be_handled, aff, literal);
+        handle_literal(graph_file, deductions, handled, to_be_handled, aff, -literal);
     }
-    fprintf(graph_file, "}\n");
-    fclose(graph_file);
+    if (write) {
+        fprintf(graph_file, "}\n");
+        fclose(graph_file);
+    }
+    return handled;
 }
 
-unsigned int cl_interact(std::map<unsigned int, std::set<unsigned int>> deductions, unsigned int literal) {
+unsigned int cl_interact(std::map<int, std::set<int>> deductions, Affectation aff, int literal) {
     char mode;
     unsigned int steps;
-    assert(deductions.find(literal) != deductions.end());
+    assert(deductions.find(literal) != deductions.end() || deductions.find(-literal) != deductions.end());
     std::cout << "Conflict on literal " << literal << ". Action? [g/c/s/t] ";
     while (true) {
         std::cin >> mode;
         switch (mode) {
             case 'g':
-                write_conflict_graph(deductions, literal);
+                make_conflict_graph(deductions, aff, literal, true);
                 return 1;
             case 'c':
                 return 1;
@@ -70,16 +85,16 @@ Affectation* satsolver::solve(Formula *formula) {
     int literal;
     unsigned int clause_id;
     bool contains_false_clause;
-    std::map<unsigned int, std::set<unsigned int>> deductions;
+    std::map<int, std::set<int>> deductions;
     unsigned int skip_conflicts = 1;
     while(formula->get_aff()->get_nb_unknown() != 0 && !formula->only_true_clauses(NULL)) {
         if(!WITH_WL && (literal = formula->monome(&clause_id))) {
-            deductions[abs(literal)] = formula->to_clauses_vector()[clause_id]->variables_to_set();
+            deductions[literal] = formula->to_clauses_vector()[clause_id]->whole_to_set();
             formula->deduce_true(literal, NULL); // Return value ignored, WITH_WL is false
             contains_false_clause = formula->contains_false_clause(&clause_id);
         }
         else if((literal = formula->isolated_literal(&clause_id))) {
-            deductions[abs(literal)] = formula->to_clauses_vector()[clause_id]->variables_to_set();
+            deductions[literal] = formula->to_clauses_vector()[clause_id]->whole_to_set();
             if (WITH_WL)
                 contains_false_clause = !formula->deduce_true(literal, &clause_id);
             else {
@@ -97,9 +112,9 @@ Affectation* satsolver::solve(Formula *formula) {
             }
         }
         while(contains_false_clause) {
-            deductions[abs(literal)] = formula->to_clauses_vector()[clause_id]->variables_to_set();
+            deductions[literal] = formula->to_clauses_vector()[clause_id]->whole_to_set();
             if (CL_INTERACT && --skip_conflicts == 0) {
-                skip_conflicts = cl_interact(deductions, abs(literal));
+                skip_conflicts = cl_interact(deductions, formula->get_aff(), literal);
             }
             literal = formula->back() ;
             if(literal == 0)
