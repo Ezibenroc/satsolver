43c3e99b95d05472df97e7ba847173b7e4ff076e
Valentin Lorentz <progval@gmail.com>
2014-04-08 22:18:27 +0200

    Move ColorAffectation to its own file.

--- src/solvers/graphcolor.cpp
+++ src/solvers/graphcolor.cpp
@@ -12,53 +12,6 @@
 
 #define EF satsolver::ExtendedFormula
 #define SPEF std::shared_ptr<EF>
-#define get_variable_of_node_bit(node_id, bit_id) (std::to_string(node_id) + " " + std::to_string(bit_id))
-
-graphsolver::ColorAffectation::ColorAffectation(int nb_nodes, int *colors) : nb_nodes(nb_nodes), colors(colors) {
-}
-graphsolver::ColorAffectation::ColorAffectation(const graphsolver::ColorAffectation &that) : nb_nodes(that.nb_nodes), colors(that.colors) {
-}
-graphsolver::ColorAffectation& graphsolver::ColorAffectation::operator=(const graphsolver::ColorAffectation &that) {
-    nb_nodes = that.nb_nodes;
-    colors = that.colors;
-    return *this;
-}
-graphsolver::ColorAffectation::~ColorAffectation() {
-    free(this->colors);
-}
-std::string graphsolver::ColorAffectation::to_string() const {
-    std::ostringstream oss;
-    oss << "{" ;
-    for(int i = 0 ; i < this->nb_nodes ; i++) {
-        if (i)
-            oss << ", ";
-        oss << i << "=" << this->colors[i];
-    }
-    oss << "}" ;
-    return oss.str() ;
-}
-
-// Get the graph coloration from the SAT solution.
-std::shared_ptr<graphsolver::ColorAffectation> graphsolver::ColorAffectation::from_sat_solution(satsolver::Affectation *affectation, std::shared_ptr<std::map<std::string, int>> name_to_variable, int nb_nodes, int nb_bits) {
-    int *colors = (int*) malloc(sizeof(int)*nb_nodes);
-    int i, j;
-    int variable_id;
-    for (i=0; i<nb_nodes; i++) {
-        colors[i] = 0;
-        for (j=nb_bits-1; j>=0; j--) {
-            colors[i] <<= 1;
-            try {
-                variable_id = name_to_variable->at(get_variable_of_node_bit(i, j));
-                if (affectation->is_true(variable_id))
-                    colors[i]++;
-            }
-            catch (std::out_of_range) { // This variable does not occur in the formula
-                // We could also increment color[i]++, it does not matter.
-            }
-        }
-    }
-    return std::make_shared<graphsolver::ColorAffectation>(nb_nodes, colors);
-}
 
 // Get a disjonction of XORs saying “I don't want these two nodes to be the
 // same color”.
---
+++ src/structures/color_affectation.h
@@ -0,0 +1,23 @@
+#include <memory>
+#include <map>
+
+#include "structures/affectation.h"
+
+namespace graphsolver {
+
+class ColorAffectation {
+    public:
+        int nb_nodes, *colors;
+
+        ColorAffectation(int nb_nodes, int *colors);
+        ColorAffectation(const ColorAffectation &that);
+        ColorAffectation& operator=(const ColorAffectation &that);
+        ~ColorAffectation();
+        std::string to_string() const;
+
+        // Deduces the color affectation from a SAT solution and the literal
+        // names translation map.
+        static std::shared_ptr<ColorAffectation> from_sat_solution(satsolver::Affectation *affectation, std::shared_ptr<std::map<std::string, int>> name_to_variable, int nodes_count, int nb_bits);
+};
+
+}
