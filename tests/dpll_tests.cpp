7924b556c27ad88a5f5827a8aa3038e3bf50aefb
Valentin Lorentz <progval@gmail.com>
2014-04-23 12:16:13 +0200

    Mark BasicUsageWithWLWithCL as skipped.

--- tests/dpll_tests.cpp
+++ tests/dpll_tests.cpp
@@ -66,6 +66,7 @@ void DpllTests::testBasicUsageWithCL() {
     WITH_CL = false;
 }
 void DpllTests::testBasicUsageWithWLWithCL() {
+    std::cout << "\n[[ BasicUsageWithWLWithCL skipped ]]\n";
     /* TODO: Enable this
     WITH_WL = true;
     WITH_CL = true;
