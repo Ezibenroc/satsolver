159bfb3aa86f355b6458a04077b6b1aab6da8639
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-04-23 22:25:32 +0200

    Update benchmark for 3SAT.

--- benchmarks/clauses.nvar_3SAT.sh
+++ benchmarks/clauses.nvar_3SAT.sh
@@ -6,16 +6,19 @@ echo "Please provide the number of tests to perform."
 exit 1
 fi
 
-NB_TEST=$1								# nombre de tests pour chaque configuration
+NB_TEST=$1						    # nombre de tests pour chaque configuration
 DIRECTORY=/tmp						# endroit de sauvegarde des fichiers temporaires
 EXEC="./resol"						# executable
-OUTPUT=clauses.nvar_3SAT.dat
+OUTPUT_WITHWL=clauses.nvar_3SAT.with_WL.dat
+OUTPUT_WITHOUTWL=clauses.nvar_3SAT.without_WL.dat
 
-rm -f $OUTPUT
+rm -f $OUTPUT_WITHWL
+rm -f $OUTPUT_WITHOUTWL
 rm -f $DIRECTORY/output.txt
-echo "Variable_number DUMB RAND MOMS DLIS DUMB_WL RAND_WL MOMS_WL DLIS_WL" >> $OUTPUT
+echo "Variable_number DUMB RAND MOMS DLIS DUMB_CL RAND_CL MOMS_CL DLIS_CL" >> $OUTPUT_WITHOUTWL
+echo "Variable_number DUMB_WL RAND_WL MOMS_WL DLIS_WL DUMB_WL_CL RAND_WL_CL MOMS_WL_CL DLIS_WL_CL" >> $OUTPUT_WITHWL
 
-for nb in `seq 10 200`; do
+for nb in `seq 10 250`; do
 	TIME_DUMB=0
 	TIME_RAND=0
 	TIME_MOMS=0
@@ -25,6 +28,16 @@ for nb in `seq 10 200`; do
 	TIME_RAND_WL=0
 	TIME_MOMS_WL=0
 	TIME_DLIS_WL=0
+	
+	TIME_DUMB_CL=0
+	TIME_RAND_CL=0
+	TIME_MOMS_CL=0
+	TIME_DLIS_CL=0
+	
+	TIME_DUMB_WL_CL=0
+	TIME_RAND_WL_CL=0
+	TIME_MOMS_WL_CL=0
+	TIME_DLIS_WL_CL=0
 
   echo "Computing test for " $nb " variables."
 
@@ -35,43 +48,176 @@ for nb in `seq 10 200`; do
 		A=$(echo "scale=0; $nb * 4.26 / 1" | bc) # on divise par 1 afin que bc fasse l'arrondi
 		./generator -nvar $nb -nclause $A -sclause 3 -o $DIRECTORY/formula.cnf 
 
+
+
+
 		# Résolution de la formule (en vérifiant la correction de la solution)
 	
 		# Heuristique DUMB
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+            if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error DUMB."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_DUMB=$(echo "scale=3; $TIME_DUMB + $TMP" | bc)
 		# Heuristique RAND
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -rand $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -rand  $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+            if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_RAND=$(echo "scale=3; $TIME_RAND + $TMP" | bc)
 		# Heuristique MOMS
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -moms $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -moms  $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_MOMS=$(echo "scale=3; $TIME_MOMS + $TMP" | bc)
 		# Heuristique DLIS
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -dlis $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -dlis  $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_DLIS=$(echo "scale=3; $TIME_DLIS + $TMP" | bc)
 		
 		# La même chose, avec les watched literals
 		
 		# Heuristique DUMB
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_DUMB_WL=$(echo "scale=3; $TIME_DUMB_WL + $TMP" | bc)
 		# Heuristique RAND
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -rand $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -rand $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+	        if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_RAND_WL=$(echo "scale=3; $TIME_RAND_WL + $TMP" | bc)
 		# Heuristique MOMS
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -moms $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -moms $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_MOMS_WL=$(echo "scale=3; $TIME_MOMS_WL + $TMP" | bc)
 		# Heuristique DLIS
-		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -dlis $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf >> $DIRECTORY/output.txt
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -dlis $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
 		TMP=`cat $DIRECTORY/result.txt`
 		TIME_DLIS_WL=$(echo "scale=3; $TIME_DLIS_WL + $TMP" | bc)
+		
+		
+		
+		
+		#############################
+		#                           #
+		# IDEM AVEC CLAUSE LEARNING #
+		#                           #
+		#############################
+		
+		# Heuristique DUMB
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_DUMB_CL=$(echo "scale=3; $TIME_DUMB_CL + $TMP" | bc)
+		# Heuristique RAND
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -rand -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_RAND_CL=$(echo "scale=3; $TIME_RAND_CL + $TMP" | bc)
+		# Heuristique MOMS
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -moms -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_MOMS_CL=$(echo "scale=3; $TIME_MOMS_CL + $TMP" | bc)
+		# Heuristique DLIS
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -dlis -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_DLIS_CL=$(echo "scale=3; $TIME_DLIS_CL + $TMP" | bc)
+		
+		# La même chose, avec les watched literals
+		
+		# Heuristique DUMB
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_DUMB_WL_CL=$(echo "scale=3; $TIME_DUMB_WL_CL + $TMP" | bc)
+		# Heuristique RAND
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -rand -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_RAND_WL_CL=$(echo "scale=3; $TIME_RAND_WL_CL + $TMP" | bc)
+		# Heuristique MOMS
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -moms -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_MOMS_WL_CL=$(echo "scale=3; $TIME_MOMS_WL_CL + $TMP" | bc)
+		# Heuristique DLIS
+		/usr/bin/time --quiet -f'%U' -o $DIRECTORY/result.txt $EXEC -WL -dlis -CL $DIRECTORY/formula.cnf | ./check_result.py $DIRECTORY/formula.cnf 2> $DIRECTORY/error.txt
+		    if [ `cat $DIRECTORY/error.txt | wc -l` -ne 0 ]
+            then
+	            echo "Fatal error."
+	            exit 1
+            fi
+		TMP=`cat $DIRECTORY/result.txt`
+		TIME_DLIS_WL_CL=$(echo "scale=3; $TIME_DLIS_WL_CL + $TMP" | bc)		
+		
+		
+		
+		
+		
 		
 	done
 	TIME_DUMB=$(echo "scale=3; $TIME_DUMB / $NB_TEST" | bc)
@@ -83,7 +229,18 @@ for nb in `seq 10 200`; do
 	TIME_RAND_WL=$(echo "scale=3; $TIME_RAND_WL / $NB_TEST" | bc)	
 	TIME_MOMS_WL=$(echo "scale=3; $TIME_MOMS_WL / $NB_TEST" | bc)	
 	TIME_DLIS_WL=$(echo "scale=3; $TIME_DLIS_WL / $NB_TEST" | bc)	
-	echo $nb $TIME_DUMB $TIME_RAND $TIME_MOMS $TIME_DLIS $TIME_DUMB_WL $TIME_RAND_WL $TIME_MOMS_WL $TIME_DLIS_WL >> $OUTPUT
+	
+	TIME_DUMB_CL=$(echo "scale=3; $TIME_DUMB_CL / $NB_TEST" | bc)
+	TIME_RAND_CL=$(echo "scale=3; $TIME_RAND_CL / $NB_TEST" | bc)	
+	TIME_MOMS_CL=$(echo "scale=3; $TIME_MOMS_CL / $NB_TEST" | bc)	
+	TIME_DLIS_CL=$(echo "scale=3; $TIME_DLIS_CL / $NB_TEST" | bc)	
+	
+	TIME_DUMB_WL_CL=$(echo "scale=3; $TIME_DUMB_WL_CL / $NB_TEST" | bc)
+	TIME_RAND_WL_CL=$(echo "scale=3; $TIME_RAND_WL_CL / $NB_TEST" | bc)	
+	TIME_MOMS_WL_CL=$(echo "scale=3; $TIME_MOMS_WL_CL / $NB_TEST" | bc)	
+	TIME_DLIS_WL_CL=$(echo "scale=3; $TIME_DLIS_WL_CL / $NB_TEST" | bc)		
+	echo $nb $TIME_DUMB $TIME_RAND $TIME_MOMS $TIME_DLIS $TIME_DUMB_CL $TIME_RAND_CL $TIME_MOMS_CL $TIME_DLIS_CL >> $OUTPUT_WITHOUTWL
+	echo $nb $TIME_DUMB_WL $TIME_RAND_WL $TIME_MOMS_WL $TIME_DLIS_WL $TIME_DUMB_WL_CL $TIME_RAND_WL_CL $TIME_MOMS_WL_CL $TIME_DLIS_WL_CL >> $OUTPUT_WITHWL
 # fin de la boucle
 done
 
